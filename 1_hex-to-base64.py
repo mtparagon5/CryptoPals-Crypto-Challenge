de03e0b55686c3a9c1ba99d7557cdebb9073bf36
Matthew Thomas <n083336@eushc.org>
2019-04-29 15:45:14 -0400

    initial commit for CrytpoPals challenge (for fun)

---
+++ 1_hex-to-base64.py
@@ -0,0 +1,19 @@
+import codecs
+
+# function to convert hex to base64
+
+
+def hex_to_base64(hex_string):
+    return codecs.encode(codecs.decode(hex_string, 'hex'), 'base64').decode()
+
+
+# hex_string = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+# correct_result = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+
+# result = hex_to_base64(hex_string)
+
+# print(correct_result)
+# print(result)
+# Output:
+# SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+# SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
